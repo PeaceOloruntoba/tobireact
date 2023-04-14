512caa5d2e84ebac98cb4f0d9ff0ffd0498f49a8
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-04-14 11:35:06 +0100

    progress commit

---
+++ tobi/pages/components/Tobi.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tobi = () => {
+  return (
+    <>
+        <div class='bg-slate-400 items-center text-center h-full'>
+            <h1 class='font-bold text-6xl text-gray-800 pt-20'>Events and News</h1>
+            <h4 class='text-2xl text-gray-700 pb-20'>The activities held at the Home of Victory are held monthly and/or by <br/>the church's liturgical calendar, some of which include.</h4>
+        </div>
+    </>
+  )
+}
+
+export default Tobi
